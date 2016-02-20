2817458a7b7ad27e2355abbc1c9c2d66254bfc8d
Igor Mikheiko <vulsim@mail.ru>
2016-02-20 22:09:23 +0300

    First commit

---
+++ D1/main.c
@@ -0,0 +1,22 @@
+/*
+    HomeMatic D1 V1.0.0 - Copyright (C) 2016 vulsim. All rights reserved
+*/
+
+#include <stdlib.h>
+#include <string.h>
+#include <avr/io.h>
+#include <avr/delay.h>
+#include <avr/eeprom.h>
+
+#include "FreeRTOSConfig.h"
+#include "FreeRTOS.h"
+#include "task.h"
+
+void vApplicationIdleHook(void) {
+
+}
+
+int main(void) {
+
+	return 0;
+}
